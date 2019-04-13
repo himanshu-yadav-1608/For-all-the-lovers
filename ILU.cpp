3f8974fb935e60dc6ffb3c0db206b661e0077128
Himanshu Yadav <46090761+himanshu-yadav-1608@users.noreply.github.com>
2019-04-14 01:34:39 +0530

    Update ILU.cpp

--- ILU.cpp
+++ ILU.cpp
@@ -1,33 +1,33 @@
-#include<stdio.h>
-main()
+#include<iostream>
+int main()
 {
 	int i,j,k,n;
 	n=12;
-	printf("\n\n");
+	cout<<endl<<endl;
 	for(i=1;i<=n;i++)
 	{
 		for(j=1;j<=n;j++)
 		{
 			if(i==1||i==n)
-			printf("*");
+			cout<<"*";
 			else if(i>=2&&i<=(n-1)&&j>((n/2)-1)&&j<(n/2)+2)
-			printf("*");
+			cout<<"*";
 			else 
-			printf(" ");
+			cout<<" ";
 		}
-	printf("\n");
+	cout<<endl;
 	}
-	printf("\n");
+	cout<<endl;
 	for(i=1;i<=3;i++)
 	{
 		for(j=1;j<=18;j++)
 		{
 			if(j>=4-i&&j<=i+3||j>=11-i&&j<=i+10)
-			printf("*",j);
+			cout<<"*";
 			else
-			printf(" ");
+			cout<<" ";
 		}
-	printf("\n");
+	cout<<endl;
 	}
 	for(i=6;i<=12;i++)
 	{
@@ -35,23 +35,23 @@ main()
 		{
 			k=i-5;
 			if(i>=6&&j>=k+1&&j<=13-k)
-			printf("*");
+			cout<<"*";
 			else 
-			printf(" ");
+			cout<<" ";
 		}
-		printf("\n");
+		cout<<endl;
 	}
 	for(i=1;i<=9;i++)
 	{
 		for(j=1;j<=12;j++)
 		{
 			if(i<=7&&(j<=3||j>=10))
-			printf("*");
+			cout<<"*";
 			else if(i>=8&&(((j>=i-6)&&(j<=i-3))||(j>=16-i&&j<=19-i)))
-			printf("*");
+			cout<<"*";
 			else
-			printf(" ");
+			cout<<" ";
 		}
-		printf("\n");
+		cout<<endl;
 	}
 }
